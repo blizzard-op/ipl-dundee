39eb3e0a0bb1c9f7fc7a24a64a1605a21ac3183b
James <jburroughs@sfmbpjburroughs.hq.ign.com>
2013-01-10 12:08:12 -0800

    Corrected inert cuepoint creation.

--- dundee/cuepoints/inert-cue-point.go
+++ dundee/cuepoints/inert-cue-point.go
@@ -16,7 +16,7 @@ type inert_cuepoint struct {
 
 func init() {
 	err := RegisterCuePointType("inert", func(r *http.Request) (interface{}, error) {
-		return &advertisement_cuepoint{Category: "Video Cue Point", Action: "13", Label: "Ad Name", Value: "0"}, nil
+		return &inert_cuepoint{Category: "Video Cue Point", Action: "13", Label: "Ad Name", Value: "0"}, nil
 	})
 	if err != nil {
 		log.Println("Failed to register cuepoint type.\n", err)
