55a511429b3d4db52f52a7ab31a6533762b4aec7
James <jburroughs@sfmbpjburroughs.hq.ign.com>
2013-01-10 12:27:50 -0800

    Resusing advertisement-cue-point type for inert-cue-point.

--- dundee/cuepoints/inert-cue-point.go
+++ dundee/cuepoints/inert-cue-point.go
@@ -1,22 +1,13 @@
 package cuepoints
 
 import (
-	"encoding/xml"
 	"log"
 	"net/http"
 )
 
-type inert_cuepoint struct {
-	XMLName  xml.Name `xml:"data"`
-	Category string   `xml:"category"`
-	Action   string   `xml:"action"`
-	Label    string   `xml:"label"`
-	Value    string   `xml:"value"`
-}
-
 func init() {
 	err := RegisterCuePointType("inert", func(r *http.Request) (interface{}, error) {
-		return &inert_cuepoint{Category: "Video Cue Point", Action: "13", Label: "Ad Name", Value: "0"}, nil
+		return &advertisement_cuepoint{Category: "Video Cue Point", Action: "13", Label: "Ad Name", Value: "0"}, nil
 	})
 	if err != nil {
 		log.Println("Failed to register cuepoint type.\n", err)
